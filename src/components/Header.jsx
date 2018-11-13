d1ecd2206b61ab253052af21323fc0e2947f19c9
Meria Thomas and Skye Nguyen <meria@>
2018-11-13 10:41:13 -0800

    add more Header stylings

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,15 @@ import React from "react";
 import NavBar from "./Navbar";
 import SearchBar from "./SearchBar";
 function Header() {
+    const HeaderStyles={
+        borderBottom: '2px solid lightgray',
+        height: '5vh',
+        marginRight: '50px',
+        marginLeft: '50px',
+        marginTop: '50px'
+    }
     return(
-        <div>
+        <div style={HeaderStyles}>
             <NavBar/>
             <SearchBar/>
         </div>
